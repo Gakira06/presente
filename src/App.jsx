c4aa734c6ed1ee128d63056e83f06c6b98718cbd
Gabriel <gakirakiyohara@gmail.com>
2026-04-28 21:01:45 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PagePrincipal from "./pages/PagePrincipal";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/principal" element={<PagePrincipal />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
